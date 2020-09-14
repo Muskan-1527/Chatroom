09c05669dce04c9d83d1b0af4c456d520e3ce245
Muskan-1527 <agrawalm883@gmail.com>
2020-09-14 12:44:20 +0530

    Updating room and username

--- scripts/chat.js
+++ scripts/chat.js
@@ -1,13 +1,9 @@
-// adding new chat documents
-// setting up a real-time listener to get new chats
-// updating the username
-// updating the room
-
 class Chatroom {
     constructor(room, username){
       this.room = room;
       this.username = username;
       this.chats = db.collection('chats');
+      this.unsub;
     }
     async addChat(message){
       // format a chat object
@@ -23,20 +19,38 @@ class Chatroom {
       return response;
     }
     getChats(callback){
-        this.chats
-          .onSnapshot(snapshot => {
-            snapshot.docChanges().forEach(change => {
-              if(change.type === 'added'){
-                callback(change.doc.data());
-              }
-            });
-        });
+      this.unsub = this.chats
+        .where('room', '==', this.room)
+        .orderBy('created_at')
+        .onSnapshot(snapshot => {
+          snapshot.docChanges().forEach(change => {
+            if(change.type === 'added'){
+              callback(change.doc.data());
+            }
+          });
+      });
+    }
+    updateName(username){
+      this.username = username;
+    }
+    updateRoom(room){
+      this.room = room;
+      console.log('room updated');
+      if(this.unsub){
+        this.unsub();
       }
     }
-    
-    const chatroom = new Chatroom('gaming', 'shaun');
-    
-    chatroom.getChats(data => {
-      console.log(data);
-    });
+  }
   
+  const chatroom = new Chatroom('gaming', 'shaun');
+  
+  chatroom.getChats(data => {
+    console.log(data);
+  });
+  
+  setTimeout(() => {
+    chatroom.updateRoom('general');
+    chatroom.updateName('yoshi');
+    chatroom.getChats(data => console.log(data));
+    chatroom.addChat('hello');
+  }, 3000);
